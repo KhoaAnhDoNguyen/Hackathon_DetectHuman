548fd9b1d2daa25bb4dea3d36c8da64337eb1aee
KhoaAnhDoNguyen <117917211+KhoaAnhDoNguyen@users.noreply.github.com>
2024-01-21 15:43:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 from PIL import Image
 import pickle as pkl
 import numpy as np
+from skimage.feature import corner_fast
 
 IMG_SIZE = 500
 
@@ -15,13 +16,17 @@ model = pkl.load(input)
 st.header('Upload your image')
 uploaded_file = st.file_uploader("Choose an image file", type=(['png', 'jpg', 'jpeg']))
 
+def feature_fast(img):
+  fast_image = corner_fast(img, n=8, threshold=0)
+  return fast_image
+    
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Test image')
 
     if st.button('Predict'):
         image = image.resize((IMG_SIZE*IMG_SIZE*3, 1))
-        feature_vector = np.array(image)
+        feature_vector = np.array(feature_fast(image)).reshape(1, IMG_SIZE*IMG_SIZE)
         label = str((model.predict(feature_vector))[0])
 
         st.header('Result')
